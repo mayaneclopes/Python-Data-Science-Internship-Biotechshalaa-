00573d908e18cb3cde6a55ff89336b948da6d02c
unknown <mayaneclopes@gmail.com>
2026-07-17 12:47:51 -0300

    Adding loops and counters

--- DNA Sequence Analyzer (Mini Project 1)/dna_sequence_analyzer.py
+++ DNA Sequence Analyzer (Mini Project 1)/dna_sequence_analyzer.py
@@ -1,6 +1,37 @@
-nucleotides = "ATCG" #allowed caracthers 
+dna = input("Type the DNA sequence: ").upper() #creating user input
+print(dna)
 
-dna_sequence = input("Type the DNA sequence: ").upper() #creating user input
+is_valid = True #Initiating validation variable
 
-validation = all(char in nucleotides for char in dna_sequence)
-print(validation)
+for nucleotideo in dna: #validation through a for loop
+    if nucleotideo not in "ATGC": 
+        is_valid = False
+        break #once the 1st invalid char is found, the loop is finished
+
+if is_valid:  
+    print("Valid DNA sequence")
+else: 
+    print("Invalid DNA sequence")
+
+#initializing counters
+counter_A = 0
+counter_T = 0
+counter_G = 0
+counter_C = 0
+
+for nucleotideo in dna:
+    if nucleotideo == "A":
+        counter_A += 1
+    elif nucleotideo == "T":
+        counter_T += 1
+    elif nucleotideo == "G":
+        counter_G += 1
+    elif nucleotideo == "C":
+        counter_C += 1
+    else:
+        break
+
+print(f"A = {counter_A}")
+print(f"T = {counter_T}")
+print(f"G = {counter_G}")
+print(f"C = {counter_C}")
