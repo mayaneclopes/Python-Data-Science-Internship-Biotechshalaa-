109e8d105eb36e04409c6671e2b3c5a2ca00635e
unknown <mayaneclopes@gmail.com>
2026-07-17 13:20:51 -0300

    Steps 6 and 7

--- DNA Sequence Analyzer (Mini Project 1)/dna_sequence_analyzer.py
+++ DNA Sequence Analyzer (Mini Project 1)/dna_sequence_analyzer.py
@@ -10,18 +10,29 @@ for nucleotideo in dna: #validation through a for loop
 
 if is_valid:  
     print("Valid DNA sequence")
+
+    #initializing counters inside if/else to ensure it wont count invalid sequences
+    counter_A = dna.count("A")
+    counter_T = dna.count("T")
+    counter_G = dna.count("G")
+    counter_C = dna.count("C")
+
+    print(f"Total DNA lenght: {len(dna)}")
+    if len(dna) >= 10:
+        print("Long DNA Sequence")
+    else:
+        print("Short DNA Sequence")
+    print(f"A = {counter_A}")
+    print(f"T = {counter_T}")
+    print(f"G = {counter_G}")
+    print(f"C = {counter_C}")
+
+    start_codon = "ATG"
+    if start_codon in dna:
+        print(f"Start Codon found in position {dna.find("ATG") + 1}")
+    else:
+        print("Start Codon Not Found")
+
 else: 
     print("Invalid DNA sequence")
 
-#initializing counters
-counter_A = dna.count("A")
-counter_T = dna.count("T")
-counter_G = dna.count("G")
-counter_C = dna.count("C")
-
-
-print(f"Total DNA lenght: {len(dna)}")
-print(f"A = {counter_A}")
-print(f"T = {counter_T}")
-print(f"G = {counter_G}")
-print(f"C = {counter_C}")
